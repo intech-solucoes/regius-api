2213571dafbd2735a22217b40e3e06b82f8cafbd
Rony Moura <ronymmoura@gmail.com>
2019-12-05 19:45:37 -0300

    Ajustes na adesão

--- Intech.PrevSystem.Regius.API/Controllers/AdesaoController.cs
+++ Intech.PrevSystem.Regius.API/Controllers/AdesaoController.cs
@@ -4,12 +4,18 @@ using Intech.Lib.Util.Seguranca;
 using Intech.Lib.Util.Validacoes;
 using Intech.Lib.Web;
 using Intech.PrevSystem.Entidades;
+using Intech.PrevSystem.Negocio;
 using Intech.PrevSystem.Negocio.Proxy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
 using System.Transactions;
 #endregion
 
@@ -248,12 +254,13 @@ namespace Intech.PrevSystem.Regius.API.Controllers
         /// </parametros>
         /// <retorno tipo="any" />
         [HttpGet("[action]/{cdEmpresa}/{numMatricula}/{cpf}/{dataNascimento}")]
-        public IActionResult BuscarFuncionario(string cdEmpresa, string numMatricula, string cpf, DateTime dataNascimento)
+        public IActionResult BuscarFuncionario(string cdEmpresa, string numMatricula, string cpf, string dataNascimento)
         {
             try
             {
                 cpf = cpf.LimparMascara();
                 numMatricula = numMatricula.PadLeft(9, '0');
+                var dtNascimento = DateTime.ParseExact(dataNascimento, "dd.MM.yyyy", new CultureInfo("pt-BR"));
 
                 var planosVinculados = new PlanoVinculadoProxy().BuscarPorFundacaoMatricula("01", numMatricula).ToList();
 
@@ -268,10 +275,10 @@ namespace Intech.PrevSystem.Regius.API.Controllers
                     });
                 }
 
-                var funcionarioNP = new FuncionarioNPProxy().BuscarPorFundacaoEmpresaMatriculaCpfDataNascimento("01", cdEmpresa, numMatricula, cpf, dataNascimento);
+                var funcionarioNP = new FuncionarioNPProxy().BuscarPorFundacaoEmpresaMatriculaCpfDataNascimento("01", cdEmpresa, numMatricula, cpf, dtNascimento);
 
                 if (funcionarioNP == null)
-                    return Ok("null");
+                    return Ok("funcionarioNP");
 
                 if (funcionarioNP.E_MAIL == null || funcionarioNP.E_MAIL.Trim() == "")
                     return Ok("email");
@@ -373,6 +380,173 @@ namespace Intech.PrevSystem.Regius.API.Controllers
             }
         }
 
+        [HttpPost("[action]"), DisableRequestSizeLimit]
+        [Consumes("multipart/form-data")]
+        public IActionResult Upload([FromForm] FileUploadViewModel model)
+        {
+            var file = model.File;
+
+            var guid = $"{Guid.NewGuid()}.{file.FileName.Split('.').Last()}";
+
+            var documento = new AdesaoDocumentoEntidade
+            {
+                TXT_NOME_FISICO = guid,
+                TXT_TITULO = model.Nome
+            };
+
+            var diretorioUpload = Path.Combine(Environment.CurrentDirectory, "Upload");
+
+            if (file.Length > 0)
+            {
+                if (!Directory.Exists(diretorioUpload))
+                    Directory.CreateDirectory(diretorioUpload);
+
+                string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                string fullPath = Path.Combine(diretorioUpload, guid);
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                var oidArquivo = new AdesaoDocumentoProxy().Inserir(documento);
+                documento.OID_ADESAO_DOCUMENTO = oidArquivo;
+            }
+
+            return Ok(documento);
+        }
+
+        /// <rota caminho="[action]/{oid}" tipo="GET" />
+        /// <parametros>
+        ///     <parametro nome="oid" tipo="number" />
+        /// </parametros>
+        /// <retorno tipo="any" />
+        [HttpGet("[action]/{oid}")]
+        public IActionResult ExcluirArquivo(decimal oid)
+        {
+            var arquivo = new AdesaoDocumentoProxy().BuscarPorChave(oid);
+            if (arquivo != null)
+            {
+                var caminhoArquivo = Path.Combine(Environment.CurrentDirectory, "Upload", arquivo.TXT_NOME_FISICO);
+                System.IO.File.Delete(caminhoArquivo);
+
+                new AdesaoDocumentoProxy().Deletar(arquivo);
+            }
+
+            return Ok();
+        }
+
+        /// <rota caminho="[action]" tipo="POST" />
+        /// <parametros>
+        ///     <parametro nome="adesao" tipo="AdesaoEntidade" />
+        /// </parametros>
+        /// <retorno tipo="string" />
+        [HttpPost("[action]")]
+        public IActionResult Inserir(AdesaoEntidade adesao)
+        {
+            using (var transaction = new TransactionScope())
+            {
+                try
+                {
+                    adesao.DTA_CRIACAO = DateTime.Now;
+                    adesao.IND_SIT_ADESAO = "";
+                    adesao.COD_CPF = adesao.COD_CPF.LimparMascara();
+                    adesao.COD_CEP = adesao.COD_CEP.LimparMascara();
+
+                    var oidAdesao = new AdesaoProxy().Inserir(adesao);
+
+                    adesao.Plano.OID_ADESAO = oidAdesao;
+                    var oidAdesaoPlano = new AdesaoPlanoProxy().Inserir(adesao.Plano);
+
+                    adesao.Contrib.OID_ADESAO_PLANO = oidAdesaoPlano;
+                    var oidAdesaoContrib = new AdesaoContribProxy().Inserir(adesao.Contrib);
+
+                    foreach (var dep in adesao.Dependentes)
+                    {
+                        dep.OID_ADESAO = oidAdesao;
+                        dep.COD_CPF = dep.COD_CPF.LimparMascara();
+
+                        new AdesaoDependenteProxy().Inserir(dep);
+                    }
+
+                    foreach(var doc in adesao.Documentos)
+                    {
+                        var documento = new AdesaoDocumentoProxy().BuscarPorChave(doc.OID_ADESAO_DOCUMENTO);
+                        documento.OID_ADESAO = oidAdesao;
+                        new AdesaoDocumentoProxy().Atualizar(documento);
+                    }
+
+                    var listaConteudo = new List<ItemTransacao>
+                    {
+                        new ItemTransacao("Nome", adesao.NOM_PESSOA),
+                        new ItemTransacao("Data de Nascimento", adesao.DTA_NASCIMENTO.ToString("dd/MM/yyyy")),
+                        new ItemTransacao("CPF", adesao.COD_CPF),
+                        new ItemTransacao("E-mail", adesao.COD_EMAIL),
+                        new ItemTransacao("Patrocinadora", adesao.DES_EMPRESA),
+                        new ItemTransacao("Matrícula", adesao.COD_MATRICULA),
+
+                        new ItemTransacao("Data de Admissão", adesao.DTA_ADMISSAO.ToString("dd/MM/yyyy")),
+                        new ItemTransacao("Sexo", adesao.DES_SEXO),
+                        new ItemTransacao("Nacionalidade", adesao.DES_NACIONALIDADE),
+                        new ItemTransacao("Naturalidade", adesao.DES_NATURALIDADE),
+                        new ItemTransacao("UF", adesao.DES_END_UF),
+                        new ItemTransacao("RG", adesao.COD_RG),
+                        new ItemTransacao("Órgão Expeditor", adesao.DES_ORGAO_EXPEDIDOR),
+                        new ItemTransacao("Emissão", adesao.DTA_EXPEDICAO_RG.Value.ToString("dd/MM/yyyy")),
+                        new ItemTransacao("Estado Civil", adesao.DES_ESTADO_CIVIL),
+                        new ItemTransacao("Nome da Mãe", adesao.NOM_MAE),
+                        new ItemTransacao("Nome do Pai", adesao.NOM_PAI),
+                        new ItemTransacao("CEP", adesao.COD_CEP),
+                        new ItemTransacao("Endereço", adesao.DES_END_LOGRADOURO),
+                        new ItemTransacao("Numero", adesao.DES_END_NUMERO),
+                        new ItemTransacao("Complemento", adesao.DES_END_COMPLEMENTO),
+                        new ItemTransacao("Bairro", adesao.DES_END_BAIRRO),
+                        new ItemTransacao("Cidade", adesao.DES_END_CIDADE),
+                        new ItemTransacao("UF", adesao.DES_END_UF),
+                        new ItemTransacao("Telefone Fixo", adesao.COD_TELEFONE_FIXO),
+                        new ItemTransacao("Telefone Celular", adesao.COD_TELEFONE_CELULAR),
+                        new ItemTransacao("Pessoa Politicamente Exposta", adesao.IND_PPE),
+                        new ItemTransacao("Familiar Politicamente Exposto", adesao.IND_PPE_FAMILIAR),
+                        new ItemTransacao("US Person", adesao.IND_FATCA),
+                        new ItemTransacao("Regime Tributação", adesao.Plano.IND_REGIME_TRIBUTACAO),
+                        new ItemTransacao("Percentual Contribuição", adesao.Contrib.VAL_CONTRIBUICAO.ToString()),
+                    };
+
+                    foreach(var dep in adesao.Dependentes)
+                    {
+                        listaConteudo.Add(new ItemTransacao("Nome", dep.NOM_DEPENDENTE));
+                        listaConteudo.Add(new ItemTransacao("CPF", dep.COD_CPF));
+                        listaConteudo.Add(new ItemTransacao("Grau Parentesco", dep.DES_GRAU_PARENTESCO));
+                        listaConteudo.Add(new ItemTransacao("Percentual Rateio", dep.COD_PERC_RATEIO.ToString()));
+                        listaConteudo.Add(new ItemTransacao("Sexo", dep.DES_SEXO));
+                        listaConteudo.Add(new ItemTransacao("Data de Nascimento", dep.DTA_NASCIMENTO.ToString()));
+                        listaConteudo.Add(new ItemTransacao("Pensão", dep.IND_PENSAO));
+                    }
+
+                    var conteudo = ProtocoloHelper.MontarConteudo(listaConteudo);
+
+                    var funcionalidade = new FuncionalidadeProxy().BuscarPorChave(18);
+                    var protocolo = ProtocoloHelper.Criar(funcionalidade.OID_FUNCIONALIDADE, "01", adesao.COD_EMPRESA, adesao.Plano.COD_PLANO, adesao.COD_MATRICULA, null, conteudo,
+                                                          adesao.NOM_PESSOA, null, adesao.IPV4, adesao.IPV4, adesao.IPV6, "", "PORTAL");
+
+                    transaction.Complete();
+
+                    var emailConfig = AppSettings.Get().Email;
+                    var texto = $"Prezado {adesao.NOM_PESSOA},<br/>" +
+                        $"Obrigado por solicitar a adesão ao plano {adesao.Plano.DES_PLANO}!<br/>" +
+                        $"Sua solicitação está em análise e em breve será processada.<br/>" +
+                        $"Atenciosamente," +
+                        $"Equipe Regius";
+                    EnvioEmail.Enviar(emailConfig, adesao.COD_EMAIL, "REGIUS - Adesão On Line", texto);
+
+                    return Ok(protocolo);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+
         #region Rotas de Validação
 
         /// <rota caminho="[action]/{cpf}" tipo="GET" />
@@ -441,49 +615,16 @@ namespace Intech.PrevSystem.Regius.API.Controllers
         }
 
         #endregion
+    }
 
-        /// <rota caminho="[action]" tipo="POST" />
-        /// <parametros>
-        ///     <parametro nome="adesao" tipo="AdesaoEntidade" />
-        /// </parametros>
-        /// <retorno tipo="string" />
-        [HttpPost("[action]")]
-        public IActionResult Inserir(AdesaoEntidade adesao)
-        {
-            using (var transaction = new TransactionScope())
-            {
-                try
-                {
-                    adesao.DTA_CRIACAO = DateTime.Now;
-                    adesao.IND_SIT_ADESAO = "";
-                    adesao.COD_CPF = adesao.COD_CPF.LimparMascara();
-                    adesao.COD_CEP = adesao.COD_CEP.LimparMascara();
-
-                    var oidAdesao = new AdesaoProxy().Inserir(adesao);
-
-                    adesao.Plano.OID_ADESAO = oidAdesao;
-                    var oidAdesaoPlano = new AdesaoPlanoProxy().Inserir(adesao.Plano);
-
-                    adesao.Contrib.OID_ADESAO_PLANO = oidAdesaoPlano;
-                    var oidAdesaoContrib = new AdesaoContribProxy().Inserir(adesao.Contrib);
-
-                    foreach (var dep in adesao.Dependentes)
-                    {
-                        dep.OID_ADESAO = oidAdesao;
-                        dep.COD_CPF = dep.COD_CPF.LimparMascara();
-
-                        new AdesaoDependenteProxy().Inserir(dep);
-                    }
-
-                    transaction.Complete();
-
-                    return Ok();
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
-            }
-        }
+    public class FileUploadViewModel
+    {
+        public IFormFile File { get; set; }
+        public string source { get; set; }
+        public string Nome { get; set; }
+        public long Size { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Extension { get; set; }
     }
 }
